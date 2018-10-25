7bbc9cac7265eda17469c190a6f14181956fa5a1
SM <212726089@logon.ds.ge.com>
2018-10-25 14:25:59 +0530

    Adding index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+// exports.getAuthenctionToken = function () {
+//     return "auth token";
+// }
+
+let getAuthenctionToken = function () {
+    console.log("ss");
+        return "auth token";
+}
+
+module.exports = {getAuthenctionToken};
